5709fb26f78cfcdbce7b63b0539be0b98286266b
plaskin <laskinp@gmail.com>
2014-03-06 14:45:15 -0800

    Added role cron

--- roles/base-ubuntu.rb
+++ roles/base-ubuntu.rb
@@ -1,4 +1,5 @@
 # roles/base-ubuntu.rb
 name "base-ubuntu"
 description "Base role for Ubuntu Servers"
-run_list "recipe[apt]"
+run_list "recipe[apt::default]","recipe[chef-client::cron]"
+# could have specified recipe[apt]. This would grab the default recipe. 
